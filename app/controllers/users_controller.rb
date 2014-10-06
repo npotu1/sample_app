2df4f386e1ce4d341aa6aad68dbee432fd014d82
npotu1 <npotu1@umbc.edu>
2014-10-06 01:19:37 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
 	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_filter :restrict,       only: [:new, :create]
   def index
      @users = User.paginate(page: params[:page])
   end
   def show
-		@user = User.find(params[:id])
-	end
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
   def new
   	@user = User.new
   end
@@ -50,12 +52,12 @@ def create
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+    # def signed_in_user
+     # unless signed_in?
+     #   store_location
+      #  redirect_to signin_url, notice: "Please sign in."
+      # end
+    #end
      def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
@@ -63,6 +65,9 @@ def create
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+    def restrict
+      redirect_to(root_url) if !current_user?(@user)
     end
 
 end
